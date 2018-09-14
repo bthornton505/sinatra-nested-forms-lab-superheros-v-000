f8273f09dde9d0d03db13558ce9f3679e9a7fc5b
Brenden Thornton <thorntonbrenden@gmail.com>
2018-09-14 17:07:52 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,14 +8,11 @@ class App < Sinatra::Base
       erb :super_hero
     end 
     
-    get '/teams' do 
-      
-    end 
     
     post '/teams' do 
       # binding.pry 
       
-      erb :team
+      erb :'app/views/team'
     end 
 
 end
